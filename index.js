24868a3c7308da13edd3791eefc29f309163114b
Nan Lin Wah Sin Htoo (Nan Nan) <121724887+nannan2025@users.noreply.github.com>
2024-01-11 00:30:41 -0500

    Adding audio
    Adding audio to the drums

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-       alert("I got clicked!");
+       var audio = new Audio("sounds/tom-1.mp3");
+       audio.play();
+       this.style.color = "white";
     });
 
 }
+
